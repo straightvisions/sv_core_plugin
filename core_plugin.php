c08de7b9386a4d1c6dcea5260bcaa89cf47d1a59
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-06-28 13:10:47 +0200

    init

--- core_plugin.php
+++ core_plugin.php
@@ -12,9 +12,54 @@
 	
 	require_once( 'core/core.php' );
 	
-	class init extends \sv_core\core {
-		const version = 4000;
-		const version_core_match = 4000;
+	class core_plugin extends core {
+		protected static $plugin_core_initialized			= false;
+		
+		protected function init_subcore(){
+			if ( !static::$plugin_core_initialized ) {
+				static::$plugin_core_initialized = true;
+				
+				add_action( 'admin_menu', array( $this, 'admin_menu' ), 1 );
+			}
+			
+			add_action( 'admin_menu', array( $this, 'section_menus' ), 1 );
+		}
+		public function admin_menu() {
+			add_menu_page(
+				__( 'straightvisions', 'sv_core' ),
+				__( 'straightvisions', 'sv_core' ),
+				'manage_options',
+				'straightvisions',
+				'',
+				$this->get_url_core( 'assets/logo_icon.png' ),
+				100
+			);
+			
+			add_submenu_page(
+				'straightvisions',				// parent slug
+				__( 'Info', 'sv_core' ),					// page title
+				__( 'Info', 'sv_core' ),					// menu title
+				'manage_options',					// capability
+				'straightvisions',				// menu slug
+				function() {								// callable function
+					$this->load_page( $this->get_path_core( 'info/backend/tpl/about.php' ) );
+				}
+			);
+		}
+		public function section_menus(){
+			foreach ( $this->get_instances() as $name => $instance ) {
+				$instance->add_section( $instance );
+				
+				add_submenu_page(
+					'straightvisions',			// parent slug
+					$instance->get_section_title(),			// page title
+					$instance->get_section_title(),			// menu title
+					'manage_options',				// capability
+					$instance->get_prefix(),				// menu slug
+					function () use ( $instance ) {			// callable function
+						$instance->load_page();
+					}
+				);
+			}
+		}
 	}
-	
-	$GLOBALS['sv_core_plugin'] = new init();
