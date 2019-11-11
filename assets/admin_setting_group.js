7d1eda65d33eb6420a97ab5fb08d4ef04bd68ac2
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-11-11 16:25:13 +0100

    SUB - UPDATE - Core + Fixed complex setting group bug and optimized id handling

--- assets/admin_setting_group.js
+++ assets/admin_setting_group.js
@@ -6,7 +6,21 @@ jQuery( 'body' ).on( 'click','.sv_setting_group_add_new_button', function() {
 	let parent 		= jQuery( this ).parents( '.sv_setting_group_parent' );
 	var form_new	= parent.find( '.sv_setting_group_new_draft' );
 	var form_clone	= form_new.clone();
-	var index		= parent.find( '.sv_setting_group' ).length;
+	var entries		= parent.find( '.sv_setting_group' );
+
+	// Only used for the entry title
+	let group_num	= entries.length;
+
+	// Checks all entries for the highest index and sets the next new index
+	let index		= -1;
+
+	entries.each( function() {
+		if ( parseInt( jQuery(this).attr('sv_setting_group_entry_id') ) > index ) {
+			index = parseInt( jQuery(this).attr('sv_setting_group_entry_id') );
+		}
+	} );
+
+	index++;
 
 	form_clone.find('.data_sv_type_sv_form_field, [data-sv_type="sv_form_field"]').each(function(e) {
 		let id = jQuery(this).attr('id').replace("sv_form_field_index", index );
@@ -22,9 +36,10 @@ jQuery( 'body' ).on( 'click','.sv_setting_group_add_new_button', function() {
 		jQuery( this ).next( 'label' ).attr( 'for', id );
 	});
 
-	form_clone.find( '.sv_setting_group_header h4' ).append(  parent.find( '.sv_setting_group_header' ).length );
+	form_clone.find( '.sv_setting_group_header h4' ).append( group_num + 1 );
 
 	form_clone.removeClass( 'sv_setting_group_new_draft' ).addClass( 'sv_setting_group' );
+	form_clone.attr( 'sv_setting_group_entry_id', index );
 	form_clone.appendTo( parent.find( '.sv_setting_group_wrapper' ) );
 
 	form_clone.show('slow');
