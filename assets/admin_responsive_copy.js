3f21325c9ecba6eb4dfa392dce2916c5bef6f2e1
Dennis Heiden <dennis-heiden@straightvisions.com>
2021-06-07 16:26:49 +0200

    add responsive settings copy support for border-radius

--- assets/admin_responsive_copy.js
+++ assets/admin_responsive_copy.js
@@ -37,6 +37,7 @@ jQuery(document).ready(function($) {
                         // do frontend update processing
                         switch(settings_type){
                             case 'border_width': SVCA.responsive_copy.update_border_width(container, id, value); break;
+                            case 'border_radius': SVCA.responsive_copy.update_border_radius(container, id, value); break;
                         }
 
                         // Settings type: color
@@ -85,6 +86,7 @@ jQuery(document).ready(function($) {
 
             self.update_border_width = function(container, id, value){
                 id = SVCA.responsive_copy.get_placeholder_id(id);
+                console.log(id);
                 const IDs = [
                     id.replace('[XXX]','\\[mobile\\]'),
                     id.replace('[XXX]','\\[mobile_landscape\\]'),
@@ -102,6 +104,39 @@ jQuery(document).ready(function($) {
                         const _value = SVCA.responsive_copy.get_value_unit_array(value);
                         const input_number = wrapper.find('.sv_setting_border_width_number');
                         const input_unit = wrapper.find('.sv_setting_border_width_unit');
+
+                        input_number.val(_value.val);
+                        input_unit.val(_value.unit);
+
+
+                    }else{
+                        console.log('ERROR - update_border_width wrapper not found!');
+                    }
+
+                }
+
+            };
+
+            self.update_border_radius = function(container, id, value){
+                id = SVCA.responsive_copy.get_placeholder_id(id);
+                console.log(id);
+                const IDs = [
+                    id.replace('[XXX]','\\[mobile\\]'),
+                    id.replace('[XXX]','\\[mobile_landscape\\]'),
+                    id.replace('[XXX]','\\[tablet\\]'),
+                    id.replace('[XXX]','\\[tablet_landscape\\]'),
+                    id.replace('[XXX]','\\[tablet_pro\\]'),
+                    id.replace('[XXX]','\\[tablet_pro_landscape\\]'),
+                    id.replace('[XXX]','\\[desktop\\]')
+                ];
+
+                for(i=0;i<IDs.length;i++){
+                    const target = container.find('.sv_input#'+IDs[i]);
+                    const wrapper = target.closest('.sv_setting_border_radius_wrapper');
+                    if(wrapper.length > 0){
+                        const _value = SVCA.responsive_copy.get_value_unit_array(value);
+                        const input_number = wrapper.find('.sv_setting_border_radius_number');
+                        const input_unit = wrapper.find('.sv_setting_border_radius_unit');
 
                         input_number.val(_value.val);
                         input_unit.val(_value.unit);
