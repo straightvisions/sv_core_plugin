0746f1870e99df861c727c86e9bb955ab72436aa
Dennis Heiden <dennis-heiden@straightvisions.com>
2021-10-12 15:19:23 +0200

    add settings basket class + simple bindings

---
+++ assets/admin_settings_basket.js
@@ -0,0 +1,64 @@
+jQuery(document).ready(function($) {
+
+    if( typeof SVCA != 'undefined' ){
+
+        SVCA.basket = new function(){
+
+            const self = this; // prevents scope problem within loops
+
+            self.params = {
+                container: null,
+                data: {}
+            };
+
+            self.call = null;
+
+            self.init = function(params){
+                console.log('SVCA-BASKET init');
+
+                self.params = {...self.params,...params};
+
+                jQuery(window).on('sv_admin_section_loaded',function(){
+                    SVCA.basket.on_update();
+                });
+
+                console.log('SVCA-BASKET init - done');
+            };
+
+            self.on_update = function(){
+                self.bind_events();
+            };
+
+            self.update = function(params){
+
+                self.params.data = {...self.params.data, ...params};
+
+                jQuery(window).trigger('SVCA_UPDATE');
+            };
+
+            self.bind_events = function(){
+              const container = self.params.container;
+
+              /* @todo detect
+              select
+              responsive copy
+              dynamic settings creation
+               */
+
+              // text, checkbox
+              jQuery(container+' input').on('change', function(){
+                  const el      = jQuery(this);
+                  const name    = el.attr('name');
+                  const value   = el.val();
+
+                  console.log(name);
+                  console.log(value);
+              });
+
+            };
+
+            // end
+        };
+       SVCA.basket.init({'container':'.sv_dashboard_content'});
+    }
+});
